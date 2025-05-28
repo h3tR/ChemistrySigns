517126fd60afba832cb5a2428f3f71ad9e24ef19
RMI <66027579+h3tR@users.noreply.github.com>
2025-05-28 12:44:15 +0200

    Server not for rendering...

--- src/main/java/chem_signs/ghs/GHSItemBlock.java
+++ src/main/java/chem_signs/ghs/GHSItemBlock.java
@@ -4,13 +4,15 @@ package chem_signs.ghs;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.FMLLaunchHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class GHSItemBlock extends ItemBlock {
     public GHSItemBlock(Block block) {
         super(block);
-        this.setTileEntityItemStackRenderer(new GHSItemRenderer());
+        if (FMLLaunchHandler.side().isClient())
+            setTEISR();
     }
 
     @Override
@@ -19,4 +21,9 @@ public class GHSItemBlock extends ItemBlock {
         return true;
     }
 
+
+    @SideOnly(Side.CLIENT)
+    public void setTEISR() {
+        this.setTileEntityItemStackRenderer(new GHSItemRenderer());
+    }
 }
--- src/main/java/chem_signs/nfpa704/NFPA704ItemBlock.java
+++ src/main/java/chem_signs/nfpa704/NFPA704ItemBlock.java
@@ -4,19 +4,26 @@ package chem_signs.nfpa704;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.FMLLaunchHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class NFPA704ItemBlock extends ItemBlock {
     public NFPA704ItemBlock(Block block) {
         super(block);
-        this.setTileEntityItemStackRenderer(new NFPA704ItemRenderer());
+        if (FMLLaunchHandler.side().isClient())
+            setTEISR();
     }
 
     @Override
     @SideOnly(Side.CLIENT)
     public boolean hasCustomEntity(ItemStack stack) {
         return true;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void setTEISR() {
+        this.setTileEntityItemStackRenderer(new NFPA704ItemRenderer());
     }
 
 }
